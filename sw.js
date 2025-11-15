34a7f3575795f77238d1ad489ad75b976da71693
kefo-tech <kefo.tech@gmail.com>
2025-11-15 12:47:22 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,40 @@
+// اسم الكاش
+const CACHE_NAME = 'kefo-work-hours-v1';
+
+// الملفات التي يتم تخزينها للعمل بدون إنترنت
+const URLS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+// تثبيت Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+// تفعيل وإزالة الكاش القديم إذا وجد
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// جلب الملفات من الكاش أولًا، ثم من الإنترنت
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
